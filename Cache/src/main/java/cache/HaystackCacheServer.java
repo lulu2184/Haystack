a7f28d1d380e7b92655d0c596708414014749db7
youx <you.xu@west.cmu.edu>
2018-04-14 01:55:40 -0700

    [Todo] Replace dir_ip&port

--- Cache/src/main/java/cache/HaystackCacheServer.java
+++ Cache/src/main/java/cache/HaystackCacheServer.java
@@ -2,7 +2,15 @@ package cache;
 
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.xnio.Options;
+
+import java.io.IOException;
 
 public class HaystackCacheServer {
 
@@ -15,5 +23,24 @@ public class HaystackCacheServer {
                 .setHandler(cacheHandler)
                 .build();
         server.start();
+
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+        manager.setMaxTotal(40);
+        manager.setDefaultMaxPerRoute(20);
+        HttpClientBuilder clientBuilder = HttpClients.custom().setConnectionManager(manager).setConnectionManagerShared(true);
+
+        // TODO: Replace dir_ip&port
+        try (CloseableHttpClient client = clientBuilder.build()) {
+            String dir_ip = "127.0.0.1";
+            String port = "4444";
+            HttpPost post = new HttpPost("http://" + dir_ip + ":" + port + "/");
+            client.execute(post);
+//            CloseableHttpResponse response = client.execute(post);
+//            int code = response.getStatusLine().getStatusCode();
+//            System.out.println(response);
+//            response.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Store/src/main/java/store/HaystackStoreServer.java
+++ Store/src/main/java/store/HaystackStoreServer.java
@@ -3,8 +3,14 @@ package store;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.form.EagerFormParsingHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.xnio.Options;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -23,6 +29,26 @@ public class HaystackStoreServer {
             server.start();
             System.out.println(inetAddress.getHostAddress());
         } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+        manager.setMaxTotal(40);
+        manager.setDefaultMaxPerRoute(20);
+        HttpClientBuilder clientBuilder = HttpClients.custom().setConnectionManager(manager).setConnectionManagerShared(true);
+
+        // TODO: Replace dir_ip&port
+        try (CloseableHttpClient client = clientBuilder.build()) {
+            String dir_ip = "127.0.0.1";
+            String port = "4444";
+            int volumn_num = 10;
+            HttpPost post = new HttpPost("http://" + dir_ip + ":" + port + "/" + volumn_num);
+            client.execute(post);
+//            CloseableHttpResponse response = client.execute(post);
+//            int code = response.getStatusLine().getStatusCode();
+//            System.out.println(response);
+//            response.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
