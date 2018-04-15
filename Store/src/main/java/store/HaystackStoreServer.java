c2f9d26a2624bf4fa690b7fbf4126e8687617f3f
youx <you.xu@west.cmu.edu>
2018-04-15 14:41:38 -0700

    [New] Send register request

--- Store/src/main/java/store/HaystackStoreServer.java
+++ Store/src/main/java/store/HaystackStoreServer.java
@@ -3,6 +3,7 @@ package store;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.form.EagerFormParsingHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -10,46 +11,60 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.xnio.Options;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class HaystackStoreServer {
 
     public static void main(String[] args) {
         HttpHandler storeHandler = new StoreHandler();
-        try {
-            InetAddress inetAddress = InetAddress.getLocalHost();
-            Undertow server = Undertow.builder()
-                    .setWorkerOption(Options.WORKER_IO_THREADS, 80)
-                    .setWorkerOption(Options.WORKER_TASK_CORE_THREADS, 400)
-                    .addHttpListener(4443, "0.0.0.0")
-                    .setHandler(new EagerFormParsingHandler().setNext(storeHandler))
-                    .build();
-            server.start();
-            System.out.println(inetAddress.getHostAddress());
-        } catch (UnknownHostException e) {
+        Undertow server = Undertow.builder()
+                .setWorkerOption(Options.WORKER_IO_THREADS, 80)
+                .setWorkerOption(Options.WORKER_TASK_CORE_THREADS, 400)
+                .addHttpListener(4443, "0.0.0.0")
+                .setHandler(new EagerFormParsingHandler().setNext(storeHandler))
+                .build();
+        server.start();
+        System.out.println(getPublicIpAddress());
+
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+        manager.setMaxTotal(40);
+        manager.setDefaultMaxPerRoute(20);
+        HttpClientBuilder clientBuilder = HttpClients.custom().setConnectionManager(manager).setConnectionManagerShared(true);
+
+        try (CloseableHttpClient client = clientBuilder.build()) {
+            String dir_ip = "128.2.13.138";
+            String port = "8080";
+//            int volumn_num = 10;
+            HttpPost post = new HttpPost("http://" + dir_ip + ":" + port + "/servers/store?dns="
+                    + getPublicIpAddress() + ":4443");
+            client.execute(post);
+            CloseableHttpResponse response = client.execute(post);
+            int code = response.getStatusLine().getStatusCode();
+            System.out.println("store register result: " + code);
+            response.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
-//        manager.setMaxTotal(40);
-//        manager.setDefaultMaxPerRoute(20);
-//        HttpClientBuilder clientBuilder = HttpClients.custom().setConnectionManager(manager).setConnectionManagerShared(true);
-//
-//        // TODO: Replace dir_ip&port
-//        try (CloseableHttpClient client = clientBuilder.build()) {
-//            String dir_ip = "127.0.0.1";
-//            String port = "4444";
-//            int volumn_num = 10;
-//            HttpPost post = new HttpPost("http://" + dir_ip + ":" + port + "/" + volumn_num);
-//            client.execute(post);
-////            CloseableHttpResponse response = client.execute(post);
-////            int code = response.getStatusLine().getStatusCode();
-////            System.out.println(response);
-////            response.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
+
+    private static String getPublicIpAddress() {
+        Process proc = null;
+        try {
+            proc = Runtime.getRuntime().exec("hostname -I");
+            InputStream in = proc.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "GBK"));
+            String ip_str = br.readLine();
+
+            String[] ips = ip_str.split(" ");
+            return ips[0];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
 }
